340e9190afd053d0384a4d3277cbb5894a8b9032
Yusuke Yamasaki <yamasaki@hogehage.com>
2021-12-12 12:54:25 +0000

    Cleaning comment

--- myled.c
+++ myled.c
@@ -23,33 +23,20 @@
 #include <linux/io.h>
 
 /* Lisence */
-MODULE_AUTHOR("Ryuichi Ueda", "Yusuke Yamasaki");
+MODULE_AUTHOR("Ryuichi Ueda, Yusuke Yamasaki");
 MODULE_DESCRIPTION("Driver for LED-Control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 /* /Lisence */
 
-/* MEMO */
-/*
- * 各コードの説明は、違うかもしれない。
- * main関数がないのは、デバドラではlinux自体がmain関数のような立場にいるから。
- * p??は、bcm2835-peripherals.pdfに対応するページ番号
- * 　そのページを見れば、値の意味がわかる(はず)
- * .koがカーネルモジュールとなる(Make file参照)
- * makeコマンドでコンパイルできるのは、カーネルモジュールのmakeがmake fileの一番目に書かれているから。
- * 　cleanは、２番目だからmakeのあとにcleanをつける必要がある。つまりmake myled.koでもイケるということ。
- * 後始末(clean内)は、作った順番の逆で進めていく。
-*/
-/* /MEMO */
-
 /* Global */
 static dev_t dev;
 static struct cdev cdv; // キャラクタデバイスの情報を格納する構造体
 static struct class *cls = NULL;
-static volatile u32 *gpio_base = NULL; // アドレスをマッピングするための配列を定義(volatile:アドレスを勝手に変えさせないためのやつなはず)
+static volatile u32 *gpio_base = NULL; // アドレスをマッピングするための配列を定義
 /* /Global */
 
-// catすると寿司を表示する無駄機能の実装(カーネル→ ユーザランド)
+// catすると寿司を表示する無駄機能の実装
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
 {
 	int size = 0;
@@ -62,14 +49,14 @@ static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* po
 	return size;
 }
 
-// デバイスファイルから(echoで書き込み)の字の読み込み(ユーザランド→ カーネル)
+// デバイスファイルから(echoで書き込み)の字の読み込み
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos) // デバイスファイルに書き込みがあった時の挙動
 {
 	char c;
 	if(copy_from_user(&c,buf,sizeof(char))) // copy_from_user:カーネルの外(=ユーザランド)からの字を読み込む
 		return -EFAULT; // error
 
-	// ON/OFFの書き込み(p90,95)
+	// ON/OFFの書き込み(z・y：全部、1・2：GPIO23、a・b：GPIO24、A・B：GPIO25)
 	if(c == 'z'){
 		gpio_base[10] = 1 << 23;
 		gpio_base[10] = 1 << 24;
@@ -103,7 +90,6 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	return 1; // 読み込んだ文字数を返す（この場合、１はダミー）このreturn 1がないと、永遠に終わらなくなる。再起必須
 }
 
-// ここのコードが大事。ファイルシステムのいろいろをやってる
 static struct file_operations led_fops = { // 挙動を書いた関数のポインタを格納する構造体(VFSから使われる)
 	.owner = THIS_MODULE,
 	.write = led_write,
@@ -115,12 +101,12 @@ static int __init init_mod(void) // カーネルモジュールの初期化
 	int retval;
 	int n;
 
-	gpio_base = ioremap_nocache(0x3f200000, 0xA0); // レジスタアドレスのリマップ(p90~91)\
-							     引数：(GPIOのレジスタの最初のアドレス,必要なアドレスの範囲(p91))
+	gpio_base = ioremap_nocache(0x3f200000, 0xA0); // レジスタアドレスのリマップ
+
 	// GPIOピンを出力にする(ピンを出力するレジスタに１を書き込む。他の値は１でないとならない)
-	// GPIO25の機能はGPFSEL2にある。GPFSEL2の17,16,15番目のビットを001に設定(p90~92らへん)
+	// GPIO25の機能はGPFSEL2にある。GPFSEL2の17,16,15番目のビットを001に設定
 	for(n = 23; n <= 25; n++){
-		const u32 led = n; // (多分)GPIOn
+		const u32 led = n; // GPIOn
 		const u32 index = led/10; // GPFSEL2(GPIO25の入出力を決めるピンが、2?番にある)
 		const u32 shift = (led%10)*3;
 		const u32 mask = ~(0x7 << shift); // 11111111111111000111111111111111(~は、反転)
@@ -129,16 +115,13 @@ static int __init init_mod(void) // カーネルモジュールの初期化
 								変えたいところだけを０にする | 変えるところに１を入れる
 	}
 
-	retval = alloc_chrdev_region(&dev, 0, 1, "myled"); // デバイス番号の取得\
-	                                                     引数：(dev(番号の入れ物)のアドレス, \
-									     ０番から１個マイナー番号が欲しい、 \
-									     デバイス名はmyled)
+	retval = alloc_chrdev_region(&dev, 0, 1, "myled"); // デバイス番号の取得
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
 		return retval;
 		
 	}
-	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev)); // MAJOR(引数):引数からメジャー番号を取り出すマクロ。ログを出す文
+	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 	
 	cdev_init(&cdv, &led_fops); // キャラクタデバイスの初期化(file_operationsを返す(led_fops))
 	retval = cdev_add(&cdv, dev, 1); // キャラクタデバイスをカーネルに登録
@@ -148,7 +131,7 @@ static int __init init_mod(void) // カーネルモジュールの初期化
 	}
 	
 	// /sys/class下にこのデバイスの情報を置く(ユーザ側から情報が見えるように)
-	cls = class_create(THIS_MODULE,"myled"); // class_createでクラスの作成(THIS_MODULE:このモジュールを管理する構造体のポインタ)
+	cls = class_create(THIS_MODULE,"myled"); // class_createでクラスの作成
 	if(IS_ERR(cls)){
 		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
@@ -162,7 +145,7 @@ static void __exit cleanup_mod(void) // 後始末
 	cdev_del(&cdv); // キャラクタデバイスの破棄
 	device_destroy(cls, dev); // デバイスの削除
 	class_destroy(cls); // クラスの削除
-	unregister_chrdev_region(dev, 1); // デバイス番号の開放(これがないとinsmodの毎に番号が増えていく)
+	unregister_chrdev_region(dev, 1); // デバイス番号の開放
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev)); // ログを吐く
 }
 
